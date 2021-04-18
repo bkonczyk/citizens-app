8638cd792a4614a8dbda09988c05e6f5c7dbcf8b
Bartosz Kończyk <bartkon@gmail.com>
2021-04-18 12:56:24 +0200

    save

--- src/main/java/pl/sda/spring/citizensapp/person/PersonService.java
+++ src/main/java/pl/sda/spring/citizensapp/person/PersonService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
